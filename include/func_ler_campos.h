5fb36d0370e5edbfa555acddab936f8811519d6f
Anthonny Gabriell Marins Alves <anthonny14gabriell@gmail.com>
2026-04-03 12:31:14 -0400

    feat(func_ler_campos): declarando funções para leitura dos campos do paciente

---
+++ include/func_ler_campos.h
@@ -0,0 +1,16 @@
+/*
+ * @Autor: Anthonny Gabriell Marins Alves
+ * @Descrição: Arquivo de cabeçalho para definir as funções de leitura dos campos do paciente durante cadastro.
+ */
+#ifndef SISTEMA_CADASTRO_DE_PACIENTES_FUNC_LER_CAMPOS_H
+#define SISTEMA_CADASTRO_DE_PACIENTES_FUNC_LER_CAMPOS_H
+#include <time.h>
+
+char* ler_nome_paciente();
+char* ler_cpf_paciente();
+int ler_origem_paciente();
+time_t ler_data_entrada_paciente();
+char* ler_nome_medico_paciente();
+char* ler_observacao_inicial_paciente();
+
+#endif //SISTEMA_CADASTRO_DE_PACIENTES_FUNC_LER_CAMPOS_H
