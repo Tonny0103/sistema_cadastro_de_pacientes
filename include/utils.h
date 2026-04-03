774b7c91a8e1fd4b08352cd9a193e970dc408941
Anthonny Gabriell Marins Alves <anthonny14gabriell@gmail.com>
2026-04-03 15:59:21 -0400

    feat(utils): declarando função de limpar buffer de entrada

---
+++ include/utils.h
@@ -0,0 +1,11 @@
+/*
+ * @Autor: Anthonny Gabriell Marins Alves
+ * @Descrição: Arquivo de cabeçalho para definir as funções utilitárias do sistema.
+ */
+
+#ifndef SISTEMA_CADASTRO_DE_PACIENTES_UTILS_H
+#define SISTEMA_CADASTRO_DE_PACIENTES_UTILS_H
+
+void limpar_buffer_entrada();
+
+#endif //SISTEMA_CADASTRO_DE_PACIENTES_UTILS_H
