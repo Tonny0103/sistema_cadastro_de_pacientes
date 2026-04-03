9e06d14aa1978672463952d0370b0de43cedcbe4
Anthonny Gabriell Marins Alves <anthonny14gabriell@gmail.com>
2026-04-03 17:29:43 -0400

    feat(utils): implementando função tentar novamente

--- src/utils.c
+++ src/utils.c
@@ -5,3 +5,8 @@ void limpar_buffer_entrada() {
     int c;
     while ((c = getchar()) != '\n' && c != EOF);
 }
+
+void tentar_novamente() {
+    printf("Pressione qualquer tecla para tentar novamente...\n");
+    getchar();
+}
