de2ecbedfe22bbb809992bed183ad24f7588b696
Anthonny Gabriell Marins Alves <anthonny14gabriell@gmail.com>
2026-04-04 10:51:43 -0400

    fix(paciente): ajustes no arquivo de cabeçalho

--- include/paciente.h
+++ include/paciente.h
@@ -8,17 +8,16 @@
 
 typedef struct {
     int id;
-    char nome[40];
-    char cpf[11];
+    char* nome;
+    char* cpf;
     int origem; // 1 - Sistema público; 2 - Sistema privado; 3 - Plano de saúde; 4 - Outros convênios
-    struct tm data_entrada;
-    char nome_medico[40];
-    char *observacao_inicial;
-} paciente;
+    time_t data_entrada;
+    char* nome_medico;
+    char* observacao_inicial;
+} t_paciente;
 
 void cadastrar_paciente();
-void editar_paciente();
 void listar_pacientes();
-void excluir_paciente();
+void buscar_pacientes();
 
 #endif //SISTEMA_CADASTRO_DE_PACIENTES_PACIENTE_H
