f8184f8dbb3e360502e162b146c0915c18686e3a
Anthonny Gabriell Marins Alves <anthonny14gabriell@gmail.com>
2026-04-04 10:26:13 -0400

    feat(utils): implementando função ler string

--- src/utils.c
+++ src/utils.c
@@ -1,6 +1,7 @@
 #include "../include/utils.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 void limpar_buffer_entrada() {
     int c;
@@ -18,4 +19,29 @@ void limpar_console() {
     #else
         system("clear");
     #endif
+}
+
+char* ler_string(int tamanho_string) {
+    char* string = (char*) malloc((tamanho_string + 1) * sizeof(char));
+
+    if (fgets(string, tamanho_string + 2, stdin) == NULL) {
+        printf("Erro 0 - Problema ao ler entrada do usuario\n");
+        free(string);
+        return NULL;
+    }
+
+    size_t tamanho = strlen(string);
+
+    if (tamanho == (tamanho_string + 1) && string[tamanho_string] != '\n') {
+        printf("Erro 1 - Numero de caracteres excede o limite (%d)!\n", tamanho_string);
+
+        limpar_buffer_entrada();
+
+        free(string);
+        return NULL;
+    }
+
+    string[strcspn(string, "\n")] = '\0';
+
+    return string;
 }
