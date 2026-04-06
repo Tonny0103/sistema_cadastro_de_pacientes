256db2d4322d274f72a2a0269e40a25fe39c4147
Anthonny Gabriell Marins Alves <anthonny14gabriell@gmail.com>
2026-04-05 20:13:10 -0400

    feat(metodo_busca): implementadas funções de busca

---
+++ include/metodo_busca.h
@@ -0,0 +1,9 @@
+#ifndef SISTEMA_CADASTRO_DE_PACIENTES_METODO_BUSCA_H
+#define SISTEMA_CADASTRO_DE_PACIENTES_METODO_BUSCA_H
+#include "comparar.h"
+#include "paciente.h"
+
+t_paciente* busca_linear(t_paciente* pacientes, int n, t_paciente alvo, t_comparar comparar);
+int busca_binaria(t_paciente* pacientes, int esquerda, int direita, t_paciente alvo, t_comparar comparar);
+
+#endif //SISTEMA_CADASTRO_DE_PACIENTES_METODO_BUSCA_H
---
+++ src/metodo_busca.c
@@ -0,0 +1,27 @@
+#include "../include/metodo_busca.h"
+
+t_paciente* busca_linear(t_paciente* pacientes, int n, t_paciente alvo, t_comparar comparar) {
+    for (int i = 0; i < n; i++) {
+        if (comparar(pacientes[i], alvo) == 0)
+            return &pacientes[i];
+    }
+
+    return NULL;
+}
+
+int busca_binaria(t_paciente* pacientes, int esquerda, int direita, t_paciente alvo, t_comparar comparar) {
+    if (esquerda > direita) {
+        return -1;
+    }
+
+    int meio = (esquerda + direita) / 2;
+    int resultado = comparar(pacientes[meio], alvo);
+
+    if (resultado == 0) {
+        return meio;
+    } else if (resultado < 0) {
+        return busca_binaria(pacientes, meio + 1, direita, alvo, comparar);
+    } else {
+        return busca_binaria(pacientes, esquerda, meio - 1, alvo, comparar);
+    }
+}
