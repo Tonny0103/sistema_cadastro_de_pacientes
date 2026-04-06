7deb0222258a70c8b37e68e52a9096b0147c1a9f
Anthonny Gabriell Marins Alves <anthonny14gabriell@gmail.com>
2026-04-05 20:41:51 -0400

    feat(comparar): criado função de comparar nomes iguais

--- src/comparar.c
+++ src/comparar.c
@@ -35,6 +35,10 @@ int comparar_medico_paciente_crescente(t_paciente a, t_paciente b) {
     return strcmp(chave_a, chave_b);
 }
 
+int comparar_nomes_iguais(t_paciente a, t_paciente b) {
+    return strcmp(a.nome, b.nome) == 0;
+}
+
 int comparar_cpf_paciente(t_paciente a, t_paciente b) {
     return strcmp(a.cpf, b.cpf) == 0;
 }
